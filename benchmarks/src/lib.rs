0845978534444d6859c6070028d0c0dd91c24014
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-04-03 16:58:56 +0900

    benchmarks: Add msgpack benchmark

--- benchmarks/src/lib.rs
+++ benchmarks/src/lib.rs
@@ -8,10 +8,23 @@ mod tests {
     use test::Bencher;
     use fruently::fluent::Fluent;
     use std::collections::HashMap;
-    use fruently::forwardable::JsonForwardable;
 
     #[bench]
-    fn benchmark_fruently(b: &mut Bencher) {
+    fn benchmark_json_forwardable(b: &mut Bencher) {
+        use fruently::forwardable::JsonForwardable;
+
+        let mut obj: HashMap<String, String> = HashMap::new();
+        obj.insert("name".to_string(), "fruently".to_string());
+        b.iter(|| {
+            let fruently = Fluent::new("0.0.0.0:24224", "test");
+            let _ = fruently.post(&obj);
+        });
+    }
+
+    #[bench]
+    fn benchmark_msgpack_forwardable(b: &mut Bencher) {
+        use fruently::forwardable::MsgpackForwardable;
+
         let mut obj: HashMap<String, String> = HashMap::new();
         obj.insert("name".to_string(), "fruently".to_string());
         b.iter(|| {
