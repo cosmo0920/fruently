afca7349accb02cb45fbe5a7f350a9dd66ac5ede
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-05-07 19:40:05 +0900

    bench: Add forward mode bench test case

--- benchmarks/src/lib.rs
+++ benchmarks/src/lib.rs
@@ -1,11 +1,14 @@
 #![feature(test)]
 
 extern crate test;
+extern crate time;
 extern crate fruently;
 
 #[cfg(test)]
 mod tests {
+    use test;
     use test::Bencher;
+    use time;
     use fruently::fluent::Fluent;
     use std::collections::HashMap;
 
@@ -31,5 +34,25 @@ mod tests {
             let fruently = Fluent::new("0.0.0.0:24224", "test");
             let _ = fruently.post(&obj);
         });
+    }
+
+    #[bench]
+    fn benchmark_forwardable(b: &mut Bencher) {
+        use fruently::forwardable::Forwardable;
+
+        let mut obj = HashMap::new();
+        let time = time::now().to_timespec().sec;
+        b.iter(|| {
+            let n = test::black_box(1000);
+            let hmap = (0..n).fold(&mut obj, |mut acc, _| {
+                {
+                    acc.insert("fwd", "fruently".to_string());
+                }
+                acc
+            });
+            let entry = (time, &*hmap);
+            let fruently = Fluent::new("0.0.0.0:24224", "test");
+            let _ = fruently.post(vec![(entry)]);
+        })
     }
 }
