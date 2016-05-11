8cc215950b14f7e28482866f1b0cc899e7b4fc10
Hiroshi Hatake <hatake@clear-code.com>
2016-05-11 19:03:21 +0900

    example thread: Request parallel

--- examples/thread.rs
+++ examples/thread.rs
@@ -7,9 +7,13 @@ use fruently::forwardable::JsonForwardable;
 fn main() {
     let fruently = Fluent::new("0.0.0.0:24224", "test");
     let mut obj: HashMap<String, String> = HashMap::new();
-    obj.insert("name".to_string(), "fruently".to_string());
-    let child = thread::spawn(move || {
+    obj.insert("threaded".to_string(), "fruently".to_string());
+    let threads: Vec<_> = (0..10).map(|_| {
+        let obj = obj.clone();
+        let fruently = fruently.clone();
+        thread::spawn(move || {
             let _ = fruently.post(&obj);
-    });
-    child.join().unwrap();
+        })
+    }).collect();
+    let _: Vec<_>  = threads.into_iter().map(|thread| thread.join()).collect();
 }
