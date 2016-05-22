fab5d860c99778ae9a7976715fa616e5e3b31ab8
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-05-22 17:09:07 +0900

    Remove needless returns

--- src/forwardable/msgpack.rs
+++ src/forwardable/msgpack.rs
@@ -32,7 +32,7 @@ impl<A: ToSocketAddrs> MsgpackForwardable for Fluent<A> {
         where T: Encodable
     {
         let time = time::now();
-        return self.post_with_time(record, time);
+        self.post_with_time(record, time)
     }
 
     /// Post record into Fluentd. With time version.
