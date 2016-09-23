ef426fd6b30095985170c1b2718a449918299de3
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-09-23 11:56:52 +0900

    example main: Output error

--- examples/main.rs
+++ examples/main.rs
@@ -7,5 +7,8 @@ fn main() {
     let mut obj: HashMap<String, String> = HashMap::new();
     obj.insert("name".to_string(), "fruently".to_string());
     let fruently = Fluent::new("0.0.0.0:24224", "test");
-    let _ = fruently.post(&obj);
+    match fruently.post(&obj) {
+        Err(e) => println!("{:?}", e),
+        Ok(_) => return,
+    }
 }
