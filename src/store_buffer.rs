a05d726ac00ec5b02d4ade2b7c5579993261db75
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-11-02 22:46:37 +0900

    store_buffer: Ensure append write mode when creating a buffer file

--- src/store_buffer.rs
+++ src/store_buffer.rs
@@ -1,10 +1,18 @@
 use std::fs::File;
+use std::path::PathBuf;
+use std::io;
 use std::io::Write;
 use std::fmt::Debug;
 use retry_conf::RetryConf;
 use rustc_serialize::Encodable;
 use record::FluentError;
 use forwardable::forward::Forward;
+use std::fs::OpenOptions;
+
+fn ensure_file_with_wca(path: PathBuf) -> Result<File, io::Error> {
+    let file = try!(OpenOptions::new().write(true).create(true).append(true).open(path));
+    Ok(file)
+}
 
 pub fn maybe_write_record<T>(conf: &RetryConf,
                              record: T,
@@ -15,11 +23,12 @@ pub fn maybe_write_record<T>(conf: &RetryConf,
     let store_needed = conf.clone().need_to_store();
     let store_path = conf.clone().store_path();
     if store_needed && store_path.is_some() {
-        match File::create(store_path.unwrap()) {
+        match ensure_file_with_wca(store_path.unwrap()) {
             Ok(mut f) => {
                 let mut w = Vec::new();
                 write!(&mut w, "{:?}", record).unwrap();
                 try!(f.write(&w));
+                try!(f.sync_data());
             }
             Err(e) => return Err(From::from(e)),
         }
@@ -38,11 +47,12 @@ pub fn maybe_write_records<T>(conf: &RetryConf,
     let store_needed = conf.clone().need_to_store();
     let store_path = conf.clone().store_path();
     if store_needed && store_path.is_some() {
-        match File::create(store_path.unwrap()) {
+        match ensure_file_with_wca(store_path.unwrap()) {
             Ok(mut f) => {
                 let mut w = Vec::new();
                 write!(&mut w, "{:?}", forward).unwrap();
                 try!(f.write(&w));
+                try!(f.sync_data());
             }
             Err(e) => return Err(From::from(e)),
         }
@@ -74,6 +84,26 @@ mod tests {
         let tmp = TempDir::new("fruently").unwrap().into_path().join("buffer");
         let conf = RetryConf::new().store_file(tmp.clone());
         assert!(maybe_write_record(&conf, record, FluentError::Dummy("dummy".to_string())).is_ok());
+        assert!(tmp.exists())
+    }
+
+    #[test]
+    fn test_write_record_2_times() {
+        let tag = "fruently".to_string();
+        let time = time::now();
+        let mut obj: HashMap<String, String> = HashMap::new();
+        obj.insert("name".to_string(), "fruently".to_string());
+        let record = Record::new(tag.clone(), time, obj.clone());
+        let tmp = TempDir::new("fruently").unwrap().into_path().join("buffer");
+        let conf = RetryConf::new().store_file(tmp.clone());
+        assert!(maybe_write_record(&conf, record, FluentError::Dummy("dummy".to_string())).is_ok());
+        assert!(tmp.exists());
+        let mut obj2: HashMap<String, String> = HashMap::new();
+        obj2.insert("name2".to_string(), "fruently2".to_string());
+        let record2 = Record::new(tag.clone(), time, obj2.clone());
+        let conf2 = RetryConf::new().store_file(tmp.clone());
+        assert!(maybe_write_record(&conf2, record2, FluentError::Dummy("dummy".to_string()))
+            .is_ok());
         assert!(tmp.exists())
     }
 
