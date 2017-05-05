541f7077429de6d066ee90ecbfa69f228c3926ed
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-05-05 13:31:15 +0900

    Update documentation

--- implementors/serde/ser/trait.Serialize.js
+++ implementors/serde/ser/trait.Serialize.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["fruently"] = ["impl&lt;T:&nbsp;<a class='trait' href='https://docs.serde.rs/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a>&gt; <a class='trait' href='https://docs.serde.rs/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='fruently/record/struct.Record.html' title='fruently::record::Record'>Record</a>&lt;T&gt;",];
+implementors["fruently"] = ["impl&lt;T:&nbsp;<a class='trait' href='https://docs.rs/serde/1.0.2/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a>&gt; <a class='trait' href='https://docs.rs/serde/1.0.2/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='fruently/record/struct.Record.html' title='fruently::record::Record'>Record</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
