35e585aab55961fb50c4c450c98ca86adff6c79c
Chen Weiguang <chen.weiguang@gmail.com>
2017-12-19 12:20:46 +0800

    Add impl std::error::Error for FluentError

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 //! Implement error types for fruently crate.
 
+use std::error;
+use std::fmt;
 use std::io;
 use retry;
 use serde_json;
@@ -14,6 +16,25 @@ pub enum FluentError {
     FileStored(String),
     #[doc(hidden)]
     Dummy(String),
+}
+
+impl fmt::Display for FluentError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match *self {
+            FluentError::JsonEncode(ref e) => write!(f, "Fluent JSON encode error: {}", e),
+            FluentError::MsgpackEncode(ref e) => write!(f, "Fluent msgpack encode error: {}", e),
+            FluentError::IO(ref e) => write!(f, "Fluent IO error: {}", e),
+            FluentError::Retry(ref e) => write!(f, "Fluent retry error: {}", e),
+            FluentError::FileStored(ref e) => write!(f, "Fluent file stored error: {}", e),
+            FluentError::Dummy(ref e) => write!(f, "Fluent dummy error: {}", e),
+        }
+    }
+}
+
+impl error::Error for FluentError {
+    fn description(&self) -> &str {
+        "FluentError"
+    }
 }
 
 impl From<io::Error> for FluentError {
@@ -37,5 +58,25 @@ impl From<retry::RetryError> for FluentError {
 impl From<serde_json::Error> for FluentError {
     fn from(err: serde_json::Error) -> FluentError {
         FluentError::JsonEncode(err)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate failure;
+    use super::*;
+    use self::failure::Error;
+    use std;
+
+    type Result<T> = std::result::Result<T, Error>;
+
+    #[test]
+    fn test_failure_err() {
+        let f = || -> Result<()> {
+            Err(FluentError::Dummy("".to_owned()))?;
+            Ok(())
+        };
+
+        assert!(f().is_err());
     }
 }
