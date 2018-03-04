bff82cf6b6f8a3b0ee2476309d46b815ef20f0b7
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2018-03-04 17:10:36 +0900

    Update documentation

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fruently"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"fruently/error/enum.FluentError.html\" title=\"enum fruently::error::FluentError\">FluentError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
